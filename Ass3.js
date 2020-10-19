9df65c6e3e127fa1b191b381aad223018c7bf79c
TriveniChaudhari <73111974+TriveniChaudhari@users.noreply.github.com>
2020-10-19 17:14:15 +0530

    Add files via upload

---
+++ Ass3.js
@@ -0,0 +1,25 @@
+var exp=require('express');
+
+var app=exp();
+
+app.listen(8100,function(){
+	console.log("Server Started")
+});
+
+app.get('/login',function(req,res){
+	var id=req.query.userid;
+	var pass=req.query.pass;
+	if(id=="object"&&pass=="knowit")
+	{
+			res.send("<h1>welecome to login page</h1>");
+	}
+	else
+	{
+		if(id!="object"&&pass!="knowit")
+			res.send("<h1 style='color:red'>Wrong Id and Password</h1>");
+		else if(id!="object")
+			res.send("<h1 style='color:red'>Wrong userid</h1> ");
+		else
+			res.send("<h1 style='color:red'>Wrong Password</h1> ");
+	}
+});
